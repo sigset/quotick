90ec00dae3aec4f86ed85578c07842aa0ecd4688
19h <kenan@sig.dev>
2021-08-29 16:41:17 +0200

    First-level export epoch, frame, frameset, quotick, tick, backing files

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-pub use tick::Tick;
-
 pub mod backing;
 pub mod config;
 pub mod epoch;
@@ -9,3 +7,11 @@ pub mod frameset;
 pub mod path_builder;
 pub mod quotick;
 pub mod tick;
+
+pub use backing::backing_file::BackingFile;
+pub use backing::random_access_file::RandomAccessFile;
+pub use epoch::Epoch;
+pub use frame::Frame;
+pub use frameset::FrameSet;
+pub use quotick::Quotick;
+pub use tick::Tick;
